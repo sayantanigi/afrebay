1828e977ac134eeae6290fa031f659eb389bef5c
ANAXA-Design <arunaksha@goigi.in>
2023-05-08 13:40:45 +0530

    Update Banner in career_tip.php

--- application/views/frontend/career_tip.php
+++ application/views/frontend/career_tip.php
@@ -1,13 +1,15 @@
+<?php 
+ if(!empty($get_banner->image) && file_exists('uploads/banner/'.$get_banner->image)){
+     $banner_img=base_url("uploads/banner/".$get_banner->image);
+            } else{
+       $banner_img=base_url("assets/images/resource/mslider1.jpg");
+        } ?>
 <section class="overlape">
-
     <div class="block no-padding">
-
         <div data-velocity="-.1"
-            style="background: url(images/resource/mslider1.jpg) repeat scroll 50% 422.28px transparent;"
+            style="background: url('<?= $banner_img ?>') repeat scroll 50% 422.28px transparent;"
             class="parallax scrolly-invisible no-parallax"></div>
-
         <!-- PARALLAX BACKGROUND IMAGE -->
-
         <div class="container fluid">
             <div class="row">
                 <div class="col-lg-12">
