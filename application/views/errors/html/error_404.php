3cb229047e4db74f5956d9e80588c5c60210e030
sayantan bhakta <sayantan@goigi.in>
2023-10-13 17:48:18 +0530

    update code

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,64 +1,106 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
+	<meta charset="utf-8">
+	<link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+	<title>404 Page Not Found</title>
+	<style type="text/css">
+		body {
+			margin: 0;
+		}
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+		.page_404 {
+			height: 100vh;
+			background: #fff;
+			font-family: 'Arvo', serif;
+		}
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+		.page_404 img {
+			width: 100%;
+		}
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+		.four_zero_four_bg {
+			background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+			height: 400px;
+			background-position: center;
+			background-repeat: no-repeat;
+		}
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+		.four_zero_four_bg h3 {
+			font-size: 80px;
+		}
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+		.link_404 {
+			color: #fff !important;
+			padding: 10px 20px;
+			background: #39ac31;
+			margin: 20px 0;
+			display: inline-block;
+		}
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
+		.contant_box_404 {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+		}
 
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+		.contant_box_404 h3 {
+			margin: 0;
+			font-family: "Open Sans";
+		}
+
+		.contant_box_404 p {
+			margin: 10px 0;
+			font-family: "Open Sans";
+		}
+
+		.contant_box_404 .link_404 {
+			margin: 10px 0;
+			border-radius: 50px;
+			background: linear-gradient(180deg, rgba(249, 80, 30, 1) 0%, rgba(252, 119, 33, 1) 100%);
+			border: 0;
+			text-decoration: none;
+		}
+
+		.Data_Block {
+			height: 100vh;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		.Data_Block .Data_Container {
+			width: 100%;
+		}
+	</style>
 </head>
+
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+	<section class="page_404">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-12 ">
+					<div class="col-sm-10 col-sm-offset-1 text-center Data_Block">
+						<div class="Data_Container">
+							<div class="four_zero_four_bg"></div>
+							<div class="contant_box_404">
+								<h3 class="h2">
+									Look like you're lost
+								</h3>
+								<p>The page you are looking for not avaible!</p>
+								<a href="<?php echo config_item('base_url'); ?>" class="link_404">Go to Home</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </body>
+
 </html>
